178de87524b1ac879251dc672073e39a99f1a047
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-02-27 18:11:39 +0530

    Setting Timers & Managing State

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 export default function TimeChallenge({ title  , targetTime }) {
+
+    const [timerStarted , setTimerStarted] = useState(false);
+    const [timerExpired , setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimeout(() => {
+            setTimerExpired(true);
+        },targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        //How do we get access to timer in this function ?
+        //The answer is with the help of refs
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost !</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className="">
-                Time is running...  / Timer inactive
+            <p className = {timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
             </p>
         </section>
     );
